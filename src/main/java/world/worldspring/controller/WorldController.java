144524fa52770f870da30cfc5841060d1e52c20d
wogur0824 <wogur0824@naver.com>
2022-04-23 17:39:34 +0900

    스프링 웹 개발 기초 방식 1. 정적콘텐츠: 그냥 파일 그대로 내려준다. 2. 템플릿 엔진(mvc): 템플릿 엔진을 모델, 뷰, 컨트롤러로 쪼개서 뷰를 템플릿엔진으로 html을 좀 더 프로그래밍하게 렌더링하게 된 html을 클라이언트한테 전달해준다. 2-1. @getmapping 어노테이션을 설정하면 괄호()안에 파일을 templates안에 있는 파일을 찾아 웹브라우저에 반환한다. 3. API방식: 객체를 반환하는 것, veiw 없이 바로 httpresponse에다가 반환하는 것 3-1. @getmapping과 @responsebody 어노테이션을 설정하면 웹브라우저에서 요청을 하면 원래 템플릿엔진이면 controller에서 view로 던지지만 @responsebody 어노테이션때문에 viewresolver가 아닌 httpmessageverter에서 http가 요청한 것이 단순 문자이면 stringconverter가 동작하고 객체이면 jsoncenverter가 동작해서 http응답에 반환한다. 3-2. 객체를 json으로 바꿔주는 라이브러리 대표적인 2가지 3-2-1. jackson: spring은 jackson이 default로 기본 3-2-2. gson: google에서 개발해서 만든 것.

--- src/main/java/world/worldspring/controller/WorldController.java
+++ src/main/java/world/worldspring/controller/WorldController.java
@@ -3,6 +3,9 @@ package world.worldspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WorldController {
@@ -12,4 +15,48 @@ public class WorldController {
         model.addAttribute("data", "world!!");
         return "world"; // templates안에 있는 world라는 파일을 찾아서 world로 렌더링해라
     }
+
+    @GetMapping("world-mvc")
+    // 웹브라우저에서 요청을 하면 controller에서 viewresolver로 던져서 찾은다음 반환하게 한다.
+    public String worldMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "world-template";
+    }
+
+    @GetMapping("world-string")
+    @ResponseBody // http에서 header부와 body와 놔누어져 있는데 거기에서 body에 직접 넣겠다.
+    public String worldString(@RequestParam("name") String name) {
+        return "world " + name;
+        // template엔진과 차이점은 view가 없고 문자 그대로 올라간다.
+        // 이 방식은 거의 쓰이질 않는다.
+    }
+
+
+    // 지금 밑에 있는건 api방식이다. (JSON의 방식)
+    // spring도 객체를 반환하고 resonsebody로 표기를 하면 json으로 반환하는게 default로 기본으로 되어있다.
+    // 웹브라우저에서 요청을 하면 원래 템플릿엔진이면 controller에서 view로 던지지만 @responsebody 어노테이션이 붙으면 viewresolver가 아닌
+    // httpmessageconverter에서 http가 요청한것이 단순 문자이면
+    // stringconverter가 동작하고 객체이면 jsoncenverter가 동작해서 http응답에 반환한다.
+    // 객체를 json으로 바꿔주는 라이브러리 대표적인 2가지
+    // 1. jackson(spring은 jackson이 기본) 2. gson(google에서 만듬)
+    @GetMapping("world-api")
+    @ResponseBody
+    public World worldApi(@RequestParam("name") String name) {
+        World world = new World();
+        world.setName(name);
+        return world;
+    }
+
+    static class World {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
